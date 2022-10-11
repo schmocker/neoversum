b71aa8c7578d2b21911338cfe409fea9a410b36e
Tobias Schmocker <tobias.schmocker@axpo.com>
2022-10-11 21:55:13 +0200

    ad content to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {TitleComponent} from './title/title.component';
-import {HelpComponent} from './help/help.component';
-import {FooterComponent} from './footer/footer.component';
-import {ImpressumComponent} from './impressum/impressum.component';
-import {ProgramComponent} from './program/program.component';
-import {registerLocaleData} from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleComponent } from './title/title.component';
+import { HelpComponent } from './help/help.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { ProgramComponent } from './program/program.component';
+import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
-import {ErinnerungenComponent} from './erinnerungen/erinnerungen.component';
-
+import { ErinnerungenComponent } from './erinnerungen/erinnerungen.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 registerLocaleData(localeDe);
-
 
 @NgModule({
   declarations: [
@@ -29,15 +28,10 @@ registerLocaleData(localeDe);
     FooterComponent,
     ImpressumComponent,
     ProgramComponent,
-    ErinnerungenComponent
+    ErinnerungenComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
-  providers: [{provide: LOCALE_ID, useValue: 'de'}],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, NgbModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'de' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,56 @@
-import {Component, OnInit} from '@angular/core';
-import {faRocket} from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  faRocket = faRocket
+  faRocket = faRocket;
+  showNavigationIndicators = true;
+  showNavigationArrows = true;
+  slides: slide[];
 
   constructor() {
+    this.slides = this.shuffleArray(slides);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  shuffleArray(array: slide[]): slide[] {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 }
+
+interface slide {
+  img: string;
+  title: string;
+  quote: string;
+}
+
+let slides: slide[] = [
+  {
+    img: 'assets/portrait/chropfchroette_032.png',
+    title: 'Chrotte Grotte',
+    quote:
+      'Ufem Planet Chrotte Grotte het ganz e huufe Fröschli. Chlini, grossi, schöni ond wüeschti.',
+  },
+  {
+    img: 'assets/portrait/chropfchroette_035.png',
+    title: 'Esotherikerin',
+    quote: "hauptsach s'Pendel schwingt",
+  },
+  {
+    img: 'assets/portrait/chropfchroette_071.png',
+    title: 'El Capitano',
+    quote:
+      "Sicherheit und Kontrolle, mini Dame ond Herre, das esch s'Wechtigscht!",
+  },
+];
