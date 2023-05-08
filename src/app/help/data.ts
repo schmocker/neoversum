84d0c620f7ae30f2995eda1afeed7cf42772ae5b
Tobias Schmocker <tobias.schmocker@axpo.com>
2023-05-08 15:41:51 +0200

    update programm

---
+++ src/app/help/data.ts
@@ -0,0 +1,533 @@
+export let data = [
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschichtschicht (23:00-04:00)',
+    date: '2023-08-18',
+    time: '23:00 - 04:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64282?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/782678?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/782683?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/782688?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/782732?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Mittagsschicht (10:30-14:30)',
+    date: '2023-08-19',
+    time: '10:30 - 14:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64761?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787376?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787388?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787392?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Nachmittagsschicht (14:00-18:00)',
+    date: '2023-08-19',
+    time: '14:00 - 18:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64762?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787395?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787406?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787401?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787402?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Frühe Abendschicht (17:30-23:30)',
+    date: '2023-08-19',
+    time: '17:30 - 23:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64759?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787345?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787360?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787355?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschichtschicht (23:00-04:00)',
+    date: '2023-08-19',
+    time: '23:00 - 04:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64760?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787362?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787366?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787369?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787372?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Mittagsschicht (10:30-14:30)',
+    date: '2023-08-20',
+    time: '10:30 - 14:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64763?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787409?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787413?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787416?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Nachmittagsschicht (14:00-18:00)',
+    date: '2023-08-20',
+    time: '14:00 - 18:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64764?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787419?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787427?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787430?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Abendschicht (17:30-24:00)',
+    date: '2023-08-20',
+    time: '17:30 - 00:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64765?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar und Essen oben',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787438?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787443?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 2,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787450?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Abendschicht (17:30-24:00)',
+    date: '2023-08-21',
+    time: '17:30 - 00:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64776?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787587?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787597?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787602?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Abendschicht (17:30-24:00)',
+    date: '2023-08-22',
+    time: '17:30 - 00:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64777?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787613?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 2,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787629?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschicht (22:00 - 02:00)',
+    date: '2023-08-23',
+    time: '22:00 - 02:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64783?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787703?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787708?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Frühe Abendschicht (17:30-22:30 resp. 24:00)',
+    date: '2023-08-24',
+    time: '17:30 - 22:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64782?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787686?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787695?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787700?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschicht (22:00 - 02:00)',
+    date: '2023-08-24',
+    time: '22:00 - 02:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64784?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787722?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787729?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Frühe Abendschicht (17:30-23:30)',
+    date: '2023-08-25',
+    time: '17:30 - 23:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64766?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787453?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787461?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787462?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787466?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschichtschicht (23:00-04:00)',
+    date: '2023-08-25',
+    time: '23:00 - 04:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64767?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 2,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787472?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787474?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787477?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787480?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Mittagsschicht (10:30-14:30)',
+    date: '2023-08-26',
+    time: '10:30 - 14:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64768?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787484?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787488?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787491?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Nachmittagsschicht (14:00-18:00)',
+    date: '2023-08-26',
+    time: '14:00 - 18:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64769?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787495?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787499?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787502?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787505?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Frühe Abendschicht (17:30-23:30)',
+    date: '2023-08-26',
+    time: '17:30 - 23:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64770?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 2,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787512?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787518?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787524?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Späte Abendschichtschicht (23:00-04:00)',
+    date: '2023-08-26',
+    time: '23:00 - 04:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64771?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787527?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787531?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787534?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787537?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Mittagsschicht (10:30-14:30)',
+    date: '2023-08-27',
+    time: '10:30 - 14:30',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64773?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787541?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787545?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787548?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Nachmittagsschicht (14:00-18:00)',
+    date: '2023-08-27',
+    time: '14:00 - 18:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64774?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787551?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787556?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 3,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787559?hcm=selected',
+      },
+    ],
+  },
+  {
+    name: 'Badenfahrt 2023 - Abendschicht (17:30-24:00)',
+    date: '2023-08-27',
+    time: '17:30 - 00:00',
+    url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/event/64775?hcm=selected',
+    assignments: [
+      {
+        role: 'Bar unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787563?hcm=selected',
+      },
+      {
+        role: 'Bar und Essen oben',
+        num: 1,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787567?hcm=selected',
+      },
+      {
+        role: 'Essen unten',
+        num: 5,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787572?hcm=selected',
+      },
+      {
+        role: 'Nachschub',
+        num: 4,
+        url: 'https://portal.helfereinsatz.ch/neoversum/de/helper/assignment/787577?hcm=selected',
+      },
+    ],
+  },
+];
--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { data } from './data';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.scss']
+  styleUrls: ['./help.component.scss'],
 })
 export class HelpComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    console.log(data);
+    let url = 'https://portal.helfereinsatz.ch/neoversum/de/getevents';
+    this.http.get(url).subscribe(d => {
+      console.log(data);
+    });
   }
-
 }
